40aed7f125d200dc341559d9c7b8dc739be93cce
Hao Wu <haowu0802@gmail.com>
2017-02-05 00:06:45 -0800

    mid

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-java -jar KBootstrap-0.2.0-app.jar -j "-Xmx768M -XX:+UseParNewGC" -r latest
-#java -jar KBootstrap-0.2.0-app.jar -r latest
+#java -jar KBootstrap-0.2.0-app.jar -j "-Xmx768M -XX:+UseParNewGC" -r latest
+java -jar KBootstrap-0.2.0-app.jar -r latest
